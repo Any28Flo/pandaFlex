e61f4652f3018ea6e086eeb0dfb1b4989fa367e4
“Erika <foglzerika@gmail.com>
2017-05-09 15:42:58 -0500

    agregando puntos master

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,40 @@
-console.log("hola");
+var contador=0;
 var arrayBotones= document.getElementsByClassName("closebtn");
 Array.prototype.forEach.call(arrayBotones,function(elemento){
   elemento.addEventListener("click",desaparece);
 });
 
-
-
+var eventoClick= document.getElementById("columnaDerecha");
+eventoClick.addEventListener("click",desapareceColumna);
+var eventoHistoria= document.getElementById("historia");
+eventoHistoria.addEventListener("click",desapareceColumnaIzq);
+var columnas= document.getElementsByClassName("columna");
 var botonRestaura= document.getElementById("restaura");
 botonRestaura.addEventListener("click", restaura);
-var contador=0;
 function desaparece(){
-
-  this.parentNode.style.display="none";
+this.parentNode.style.display="none";
   contador++;
-  if(contador >=4)
-  {
+  if(contador >=4){
+    contador=0;
     this.parentNode.parentNode.style.display="none";
   }
-
 }
 
 function restaura(){
-
   var contenedorPandas= document.getElementsByClassName("panda");
   var contenedor= document.getElementById("contenedor");
   contenedor.style.display="flex";
   Array.prototype.forEach.call(contenedorPandas,function(elemento){
-
-
+    console.log(elemento);
     elemento.style.display="flex";
-    // this.style.display="flex";
 
   });
+}
+function desapareceColumna(){
+
+  columnas[0].style.display="none";
 
 }
-function muestraPanda()
-{
+function desapareceColumnaIzq(){
+  columnas[1].style.display="none";
 }
